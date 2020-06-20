cc18a0d98cfebc9cccff1a1e7e41385a09e992a4
200819010RolandoEstuardoSubuyujJuarez <2558610600101@ingenieria.usac.edu.gt>
2020-06-20 16:11:02 -0600

    servidor

---
+++ servidor.py
@@ -0,0 +1,37 @@
+import socket
+import sys
+
+
+HOST = ""
+PORT = 9998
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+
+print("Listening ...")
+
+
+
+while True:
+    conn, addr = s.accept()
+    print("conectado: ", addr)
+
+
+    # f_send = "200819010_servidor.wav"
+    # print(len(f_send))
+    # with open(f_send, "rb") as f:
+    #     print("[+] Sending file...")
+    #     print(f)
+    #     data = f.read()
+    #     conn.sendall(data)
+        
+    
+    # break
+
+            
+conn.close() 
+f.close()       
+print("[-] Disconnected")
+sys.exit(0)
+
